51d5f0f0849b34b6dfe1876a6a0251dc8de9ec3b
dandielo <daniel.penkala@gmail.com>
2016-03-15 00:06:23 +0100

    Updated the Potion attribute to reflect the new Potion API in CraftBukkit 1.9

--- src/main/java/net/dandielo/core/items/serialize/core/Potion.java
+++ src/main/java/net/dandielo/core/items/serialize/core/Potion.java
@@ -6,16 +6,21 @@ import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 
 import net.dandielo.core.items.dItem;
 import net.dandielo.core.items.serialize.Attribute;
 import net.dandielo.core.items.serialize.ItemAttribute;
 
-@Attribute(name="Potion", key="pt", priority = 5, items = {Material.POTION})
+@Attribute(name="Potion", key="pt", priority = 5, items = {Material.POTION, Material.SPLASH_POTION, Material.LINGERING_POTION})
 public class Potion extends ItemAttribute {
 	private List<PotionEffect> effects = new ArrayList<PotionEffect>();
+	private boolean extended;
+	private boolean upgraded;
+	private PotionType type;
 	
 	public Potion(dItem item, String key)
 	{
@@ -25,67 +30,101 @@ public class Potion extends ItemAttribute {
 	@Override
 	public boolean deserialize(String data)
 	{
-		String[] savedEffects = data.split(",");
-		for ( String savedEffect : savedEffects )
+		String[] potionData = data.split("@");
+		String[] baseData = potionData[0].split("\\.");
+		
+		// Get the potion base data
+		type = PotionType.valueOf(baseData[0]);
+		for (int i = 1; i < baseData.length; ++i)
 		{
-			String[] effectData = savedEffect.split("/");
-			PotionEffect effect = new PotionEffect(
-					PotionEffectType.getByName(effectData[0]),
-					Integer.parseInt(effectData[1]),
-					Integer.parseInt(effectData[2]),
-					Boolean.parseBoolean(effectData[3]));
-			effects.add(effect);
+			if (baseData[i].equals("ext"))
+			{
+				extended = true;
+			}
+			else if (baseData[i].equals("upg"))
+			{
+				upgraded = true;
+			}
 		}
-		return false;
+		
+		// Get the potion effects
+		if (potionData.length > 1 && potionData[1] != null && potionData[1].length() > 0)
+		{
+			String[] savedEffects = potionData[1].split(",");
+			for ( String savedEffect : savedEffects )
+			{
+				String[] effectData = savedEffect.split("/");
+				PotionEffect effect = new PotionEffect(
+						PotionEffectType.getByName(effectData[0]),
+						Integer.parseInt(effectData[1]),
+						Integer.parseInt(effectData[2]),
+						Boolean.parseBoolean(effectData[3]));
+				effects.add(effect);
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public String serialize()
 	{
-		String result = ",";
+		String result = type.toString();
+		if (extended)
+			result += ".ext";
+		if (upgraded)
+			result += ".upg";
+		result += "@";
 		
 		//save each potion effect with a comma separated
 		for ( PotionEffect e : effects )
 			result += e.getType().getName() + "/" + e.getDuration() + "/" + e.getAmplifier() + "/" + e.isAmbient();
-		
-		return result.substring(1);
+		return result;
 	}
 
 	@Override
 	public void onAssign(ItemStack item, boolean unused)
 	{
-		if ( !item.getType().equals(Material.POTION) ) return;
+		if ( !(item.getType().equals(Material.POTION) || item.getType().equals(Material.SPLASH_POTION) || 
+				item.getType().equals(Material.LINGERING_POTION)) ) return;
 
 		PotionMeta meta = (PotionMeta) item.getItemMeta();
-		
 		if (!effects.isEmpty())
 		{
 			for ( PotionEffect effect : effects )
 			    meta.addCustomEffect(effect, false);
-			
-			//set the main effect
-			meta.setMainEffect(effects.get(0).getType());
 		}
 		
+		//set the main effect
+		meta.setBasePotionData(new PotionData(type, extended, upgraded));
 		item.setItemMeta(meta);
 	}
 
 	@Override
 	public boolean onRefactor(ItemStack item)
 	{
-		if ( !item.hasItemMeta() || !item.getType().equals(Material.POTION) ) 
+		if ( !(item.getType().equals(Material.POTION) || item.getType().equals(Material.SPLASH_POTION) || 
+				item.getType().equals(Material.LINGERING_POTION)) ) 
 			return false;
 		
 		PotionMeta meta = (PotionMeta) item.getItemMeta(); 
-		
 		effects = meta.getCustomEffects(); 
+		
+		PotionData data = meta.getBasePotionData();
+		extended = data.isExtended();
+		upgraded = data.isUpgraded();
+		type = data.getType();
+
 		return true;
 	}
 
 	@Override
 	public boolean same(ItemAttribute attr)
 	{
+		if ( !type.equals(((Potion)attr).type) ) return false;
+		if ( extended != ((Potion)attr).extended ) return false;
+		if ( upgraded != ((Potion)attr).upgraded ) return false;
 		if ( ((Potion)attr).effects.size() != effects.size() ) return false;
+		
 		
 		boolean equals = true;
 		for ( PotionEffect effect : ((Potion)attr).effects )
